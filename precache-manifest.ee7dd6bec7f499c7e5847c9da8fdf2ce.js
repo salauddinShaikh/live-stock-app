960ea1bf6b2a3a35c2a2eee374e2dce97a5517a9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-06 05:45:14 +0000

    Deploy salauddinShaikh/live-stock-app to github.com/salauddinShaikh/live-stock-app.git:gh-pages

---
+++ precache-manifest.ee7dd6bec7f499c7e5847c9da8fdf2ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53e0c70a805e5c1aaa37e29ddc3bbe5",
+    "url": "/live-stock-app/index.html"
+  },
+  {
+    "revision": "887bee3ee684f6703650",
+    "url": "/live-stock-app/static/css/main.3853ca5a.chunk.css"
+  },
+  {
+    "revision": "675fb91d371971b0608e",
+    "url": "/live-stock-app/static/js/2.349baeb1.chunk.js"
+  },
+  {
+    "revision": "887bee3ee684f6703650",
+    "url": "/live-stock-app/static/js/main.b0083c8e.chunk.js"
+  },
+  {
+    "revision": "f2f3204d740b3bdd06a3",
+    "url": "/live-stock-app/static/js/runtime~main.73fdb926.js"
+  }
+]);
